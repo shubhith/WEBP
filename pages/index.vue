89f005c0fff64efd38a9f940d1c0d92417471371
Honey Thakuria <thakuria.honey@gmail.com>
2019-03-10 23:26:40 +0530

    App build

--- pages/index.vue
+++ pages/index.vue
@@ -9,8 +9,8 @@
         <div class="links">
          <a href="https://www.linkedin.com/in/honeythakuria/" target="_blank"> <i class="fab fa-linkedin"></i> </a>
          <a href="https://github.com/honey93" target="_blank"> <i class="fab fa-github"></i></a>
-         <a href="https://twitter.com/HoneyThakuria" target="_blank"> <i class="fab fa-medium"></i> </a>
-         <a href="https://medium.com/@HoneyThakuria" target="_blank"><i class="fab fa-twitter"></i> </a>
+         <a href="https://twitter.com/HoneyThakuria" target="_blank"> <i class="fab fa-twitter"></i> </a>
+         <a href="https://medium.com/@HoneyThakuria" target="_blank"><i class="fab fa-medium"></i> </a>
         </div>
       </div>
     </section>
