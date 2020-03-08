e6ddcee9601850d37f42acf7548267e3bf96fad5
Honey Thakuria <thakuria.honey@gmail.com>
2020-03-09 03:51:40 +0530

    Typing effect added to tech line

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,10 @@
           <div class="col-md-7">
             <p class="heroHello">Hello!</p>
             <p class="heroName">I’m Honey</p>
-            <p class="heroDescription">I’m a Full-Stack Developer. I enjoy building end to end system for masses.</p>
+            <p
+              class="heroDescription"
+            >I’m a Full-Stack Developer. I enjoy building end to end system for masses.</p>
+            <p class="heroDescription">I create apps using <span class="techColor"> {{this.tech}} </span> <span v-if="showTyping"> | </span> </p>
           </div>
           <div class="col-md-5">
             <img src="~/static/honeythakuria.png" class="heroImage" />
@@ -14,79 +17,90 @@
         </div>
       </div>
 
-      <div class="stuffSection"> 
-        <div class="row"> 
-          <div class="col-md-6 "> 
-            <h1 class="stuffTitle">Stuff I've done </h1>
-            <div class="stuffParent" v-for="individualStuff in $store.state.stuff" :key="individualStuff.id" v-if="individualStuff.id < 3"> 
-                <a :href="individualStuff.url" target="_blank">
+      <div class="stuffSection">
+        <div class="row">
+          <div class="col-md-6">
+            <h1 class="stuffTitle">Stuff I've done</h1>
+            <div
+              class="stuffParent"
+              v-for="individualStuff in $store.state.stuff"
+              :key="individualStuff.id"
+              v-if="individualStuff.id < 3"
+            >
+              <a :href="individualStuff.url" target="_blank">
                 <div class="stuffImage">
-                  <img :src="individualStuff.imageUrl" class="stuffImageActual"> 
-
+                  <img :src="individualStuff.imageUrl" class="stuffImageActual" />
                 </div>
-                <div class="stuffText"> 
+                <div class="stuffText">
                   <p class="stuffTextTitle">{{individualStuff.title}}</p>
-                <p class="stuffDesc"><span class="publicationText">  Publication: </span> {{individualStuff.publication}} </p>
+                  <p class="stuffDesc">
+                    <span class="publicationText">Publication:</span>
+                    {{individualStuff.publication}}
+                  </p>
                 </div>
-                </a>
-
-              </div>
+              </a>
+            </div>
           </div>
-          <div class="col-md-6" > 
-              <div class="stuffParent-right" v-for="individualStuff in $store.state.stuff" :key="individualStuff.id" v-if="individualStuff.id >= 3"> 
-                <a :href="individualStuff.url" target="_blank">
+          <div class="col-md-6">
+            <div
+              class="stuffParent-right"
+              v-for="individualStuff in $store.state.stuff"
+              :key="individualStuff.id"
+              v-if="individualStuff.id >= 3"
+            >
+              <a :href="individualStuff.url" target="_blank">
                 <div class="stuffImage">
-                  <img :src="individualStuff.imageUrl" class="stuffImageActual"> 
-
+                  <img :src="individualStuff.imageUrl" class="stuffImageActual" />
                 </div>
-                <div class="stuffText"> 
+                <div class="stuffText">
                   <p class="stuffTextTitle">{{individualStuff.title}}</p>
-                <p class="stuffDesc"> <span class="publicationText">  Publication: </span> {{individualStuff.publication}} </p>
+                  <p class="stuffDesc">
+                    <span class="publicationText">Publication:</span>
+                    {{individualStuff.publication}}
+                  </p>
                 </div>
-                </a>
-                
-
-              </div>
+              </a>
+            </div>
           </div>
         </div>
-
       </div>
 
-
-      <div class="stuffSection"> 
-        <div class="row"> 
-          <div class="col-md-6 "> 
-            <h1 class="stuffTitle">Places I've worked </h1>
-            <div class="stuffParent" v-for="individualCompany in $store.state.companies" :key="individualCompany.id" v-if="individualCompany.id < 3"> 
-                <div class="stuffImage">
-                  <img :src="individualCompany.url" class="stuffImageActual"> 
-
-                </div>
-                <div class="stuffText"> 
-                  <p class="companyTitle">{{individualCompany.name}} </p>
-                  <p class="companyDuration"> {{individualCompany.duration}} </p>
-                
-                </div>
-
+      <div class="stuffSection">
+        <div class="row">
+          <div class="col-md-6">
+            <h1 class="stuffTitle">Places I've worked</h1>
+            <div
+              class="stuffParent"
+              v-for="individualCompany in $store.state.companies"
+              :key="individualCompany.id"
+              v-if="individualCompany.id < 3"
+            >
+              <div class="stuffImage">
+                <img :src="individualCompany.url" class="stuffImageActual" />
               </div>
+              <div class="stuffText">
+                <p class="companyTitle">{{individualCompany.name}}</p>
+                <p class="companyDuration">{{individualCompany.duration}}</p>
+              </div>
+            </div>
           </div>
-          <div class="col-md-6" > 
-              <div class="stuffParent-right" v-for="individualCompany in $store.state.companies" :key="individualCompany.id" v-if="individualCompany.id >= 3"> 
-                <div class="stuffImage">
-                  <img :src="individualCompany.url" class="stuffImageActual"> 
-
-                </div>
-                <div class="stuffText"> 
-                  <p class="companyTitle">{{individualCompany.name}} </p>
-                  <p class="companyDuration"> {{individualCompany.duration}} </p>
-                
-                </div>
-                
-
+          <div class="col-md-6">
+            <div
+              class="stuffParent-right"
+              v-for="individualCompany in $store.state.companies"
+              :key="individualCompany.id"
+              v-if="individualCompany.id >= 3"
+            >
+              <div class="stuffImage">
+                <img :src="individualCompany.url" class="stuffImageActual" />
               </div>
+              <div class="stuffText">
+                <p class="companyTitle">{{individualCompany.name}}</p>
+                <p class="companyDuration">{{individualCompany.duration}}</p>
+              </div>
+            </div>
           </div>
         </div>
-
       </div>
 
       <!-- <div>
@@ -106,6 +120,29 @@ export default {
   components: {
     Logo
   },
+  mounted() {
+    let j = 0;
+    let tempArr = this.arrayTech[j].split("");
+    let i = 0;
+
+    setInterval(() => {
+      this.showTyping = !this.showTyping;
+      if (i < tempArr.length) {
+        this.type(tempArr[i]);
+        i++;
+      } else {
+        this.tech="";
+        i=0;
+        j++;
+        if (j < this.arrayTech.length) {
+          tempArr = this.arrayTech[j].split("");
+        }else{
+          j = 0;
+          tempArr = this.arrayTech[j].split("");
+        }
+      }
+    }, 350);
+  },
   head() {
     return {
       title: "Honey Thakuria",
@@ -118,169 +155,180 @@ export default {
         }
       ]
     };
+  },
+  data() {
+    return {
+      showTyping:true,
+      tech: "",
+      arrayTech: [
+        "Vue",
+        "React",
+        "Angular",
+        "Node.JS",
+        "Python",
+        "AWS",
+        "MongoDB",
+        "Javascript"
+      ]
+    };
+  },
+  methods: {
+    type(text) {
+      this.tech += text;
+    }
   }
 };
 </script>
 
 <style scoped>
-.heroHello{
-font-family: Karla;
-font-style: normal;
-font-weight: normal;
-font-size: 28px;
-line-height: 33px;
-color: #4E4E50;
+.techColor{
+  color:#3AA8FF;
 }
-.heroName{
-font-family: Karla;
-font-style: normal;
-font-weight: bold;
-font-size: 72px;
-line-height: 84px;
-letter-spacing: -0.07em;
-
-color: #000000;
-}
-
-.heroDescription{
-
+.heroHello {
   font-family: Karla;
-font-style: normal;
-font-weight: normal;
-font-size: 32px;
-line-height: 37px;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 28px;
+  line-height: 33px;
+  color: #4e4e50;
+}
+.heroName {
+  font-family: Karla;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 72px;
+  line-height: 84px;
+  letter-spacing: -0.07em;
 
-color: #4E4E50;
-
+  color: #000000;
 }
 
-.heroImage{
+.heroDescription {
+  font-family: Karla;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 32px;
+  line-height: 37px;
+
+  color: #4e4e50;
+}
+
+.heroImage {
   float: right;
 }
-.heroBanner{
-    margin-top: 267px;
-    margin-bottom: 267px; 
+.heroBanner {
+  margin-top: 267px;
+  margin-bottom: 267px;
 }
-.stuffSection{
-  margin-bottom:240px;
+.stuffSection {
+  margin-bottom: 240px;
 }
-.stuffParent{
-  width:100%;
+.stuffParent {
+  width: 100%;
   max-width: 488px;
-  
+
   position: relative;
-  margin-bottom:50px;
+  margin-bottom: 50px;
 }
 
-.height-normal{
+.height-normal {
   height: 292px;
 }
-.height-extra{
-    height: 305px;
+.height-extra {
+  height: 305px;
 }
 
-
-.stuffParent-right{
-  width:100%;
+.stuffParent-right {
+  width: 100%;
   max-width: 488px;
   position: relative;
-  margin-bottom:50px;
-  float:right;
+  margin-bottom: 50px;
+  float: right;
 }
 
-.stuffImage{
-  width:100%;
+.stuffImage {
+  width: 100%;
   max-width: 488px;
   box-shadow: 0px 8px 16px rgba(0, 0, 0, 0.24);
   height: 229px;
-  overflow:hidden;
-  border-radius:5px;
-  
+  overflow: hidden;
+  border-radius: 5px;
 }
-.stuffImageActual{
-  width:100%;
+.stuffImageActual {
+  width: 100%;
 }
-.stuffTitle{
-  margin-bottom:140px;
+.stuffTitle {
+  margin-bottom: 140px;
   font-family: Karla;
-font-style: normal;
-font-weight: bold;
-font-size: 48px;
-line-height: 56px;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 48px;
+  line-height: 56px;
 
-color: #000000;
+  color: #000000;
 }
-.stuffText{
-  padding:10px;
-  
+.stuffText {
+  padding: 10px;
 }
 .stuffText p {
   margin: 0;
 }
 
 @media screen and (max-width: 600px) {
+  .heroImage {
+    display: none;
+  }
 
-  .heroImage{
-  display: none;
-}
-
-.heroBanner{
+  .heroBanner {
     margin-top: 150px;
-    margin-bottom: 150px; 
-}
-  
+    margin-bottom: 150px;
+  }
 }
 
-.stuffTextTitle{
+.stuffTextTitle {
   font-family: Karla;
-font-style: normal;
-font-weight: bold;
-font-size: 18px;
-line-height: 21px;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 18px;
+  line-height: 21px;
 
-color: #000000;
+  color: #000000;
 }
-.stuffDesc{
+.stuffDesc {
   font-family: Karla;
-font-style: normal;
-font-weight: bold;
-font-size: 18px;
-line-height: 21px;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 18px;
+  line-height: 21px;
 
-color: #000000;
+  color: #000000;
 }
 
-.publicationText{
-  color: #4E4E50;
+.publicationText {
+  color: #4e4e50;
 }
 
-.companyTitle{
-
+.companyTitle {
   font-family: Karla;
-font-style: normal;
-font-weight: bold;
-font-size: 18px;
-line-height: 21px;
-text-align: center;
-
+  font-style: normal;
+  font-weight: bold;
+  font-size: 18px;
+  line-height: 21px;
+  text-align: center;
 }
 
-.companyDuration{
-
+.companyDuration {
   font-family: Karla;
-font-style: normal;
-font-weight: bold;
-font-size: 18px;
-line-height: 21px;
-text-align: center;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 18px;
+  line-height: 21px;
+  text-align: center;
 
-color: #4E4E50;
-
+  color: #4e4e50;
 }
 
-.stuffSection a{
+.stuffSection a {
   text-decoration: none;
 }
-
-
 </style>
